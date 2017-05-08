ffb77c34cffa7e878f6d4c91e987df2576c513be
Whitney <whitney.purdum@gmail.com>
2017-05-08 14:34:15 -0400

    update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,49 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
-})();
+  angular.module("wdinstagram", [
+    "ui.router",
+    "ngResource"
+  ])
+  .config([
+    ."$stateProvider",
+    RouterFunction
+  ])
+  .factory("EntryFactory", [
+    "$resource"
+    FactoryFunction
+  ])
+  .controller("EntryIndexController", [
+    "EntryFactory",
+    "$stateParams",
+    EntryIndexControllerFunction
+  ])
+  .controller("EntryShowController", [
+    "EntryFactory",
+    "$stateParams",
+    EntryShowControllerFunction
+  ])
+  .controller("EntryNewController", [
+    "EntryFactory",
+    "EntryNewControllerFunction"
+  ])
+  .controller("EntryEditController"[
+    "EntryFactory",
+    "$stateParams",
+    EntryEditControllerFunction
+  ])
+});
+
+function EntryFactoryFunction( $resource ){
+  return $resource ("http://localhost:3000/entries/:id", {}, {
+    update: {method: "PUT"}
+  });
+}
+
+function EntryFactoryFunction($resource){
+  return $resource("http://localhost:3000/entries/:id");
+}
+
+
+
+();
