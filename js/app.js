9942726810155c06ad897334db7031fca8cebf4a
Whitney <whitney.purdum@gmail.com>
2017-05-09 18:22:12 -0400

    update

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@
     "ngResource"
   ])
   .config([
-    ."$stateProvider",
+    "$stateProvider",
     RouterFunction
   ])
   .factory("EntryFactory", [
-    "$resource"
-    EnryFactoryFunction
+    "$resource",
+    EntryFactoryFunction
   ])
   .controller("EntryIndexController", [
     "EntryFactory",
@@ -32,28 +32,65 @@
     "$stateParams",
     EntryEditControllerFunction
   ])
-});
+
 
 function EntryFactoryFunction( $resource ){
   return $resource ("http://localhost:3000/entries/:id", {}, {
     update: {method: "PUT"}
-  });
+  // });
+  // let entries = [{
+  //     author: "Joe",
+  //     photo_url: "http://www.costafarms.com/CostaFarms/Costa-Farms-Succulent-Echeveria-peacockii.jpg?height=257&width=256&scale=both&crop=auto",
+  //     body: "Goals"
+  //   },
+  //   {
+  //       author: "Jill",
+  //       photo_url: "http://www.godofevolution.com/wordpress/wp-content/uploads/2014/01/tumblr_me5xj2Dmlr1r3clqao1_1280.jpg",
+  //       body: "Bill"
+  //     },
+  //     {
+  //       author: "Whitney",
+  //       photo_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/GrhToscane.jpg/240px-GrhToscane.jpg",
+  //       body: "Vacation"
+  //       },
+  // ]
+// }
+// return {entries}
 }
 
 function EntryShowControllerFunction(EntryFactory, $stateParams) {
-  this.entry = EntryFactory.get({id: $stateParams.id});
+  // this.entry = EntryFactory.get({id: $stateParams.id});
+  this.entry = EntryFactory.entries[]
 }
 
 function EntryIndexControllerFunction(EntryFactory){
   this.entries = EntryFactory.query(;)
+  // this.entries = [{
+  //     author: "Joe",
+  //     photo_url: "http://www.costafarms.com/CostaFarms/Costa-Farms-Succulent-Echeveria-peacockii.jpg?height=257&width=256&scale=both&crop=auto",
+  //     body: "Goals"
+  //   },
+  //   {
+  //       author: "Jill",
+  //       photo_url: "http://www.godofevolution.com/wordpress/wp-content/uploads/2014/01/tumblr_me5xj2Dmlr1r3clqao1_1280.jpg",
+  //       body: "Bill"
+  //     },
+  //     {
+  //       author: "Whitney",
+  //       photo_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/GrhToscane.jpg/240px-GrhToscane.jpg",
+  //       body: "Vacation"
+  //       },
+  // ]
 }
 
 function EntryEditControllerFunction( EntryFactory, $stateParams ) {
-  this.entry = EntryFactory.get({id: @stateParams.id});
+  this.entry = EntryFactory.get({id: $stateParams.id});
   this.update = function() {
     this.entry.save
   }
 }
+
+
 
 // author, photo URL, body
 
@@ -67,10 +104,10 @@ function RouterFunction($stateProvider){
   }).state("entryNew", {
     url: "/entries/new",
     templateUrl: "js/ng-views/new.html",
-    controller: "EntryNewController"
+    controller: "EntryNewController",
     controllerAs: "vm"
   })
-  .state(entryShow, {
+  .state("entryShow", {
     url: "/entries/:id",
     templateUrl: "js/ng-views/show.html",
     controller: "EntryShowController",
@@ -79,11 +116,9 @@ function RouterFunction($stateProvider){
     .state("entryEdit", {
       url: "/entries/:id/edit",
       templateUrl: "js/ng-views/edit.html",
-      controller: "EntryEditController"
+      controller: "EntryEditController",
       controllerAs: "vm"
     })
 }
 
-
-
-();
+})();
